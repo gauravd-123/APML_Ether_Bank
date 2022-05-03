255f6f2b25cb8dcaa7b6f89802ae7a4050dda0f2
gauravd-123 <gauravdabholkar410@gmail.com>
2022-05-03 13:58:41 +0530

    Commit

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+const { task } = require("hardhat/config");
+
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/7071090f38f2434ab6d083b63c29dac7",
+      accounts: ['0x77c6ddbc9c5cdef72fd34d7610f16aeea5dc8805b94a2a256025f443e95137ee']
+    }
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+ import { ethers } from 'ethers';
+
 import './App.css';
 
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import Token from './artifacts/contracts/token.sol/Token.json'
+
+const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+const tokenAddress =  "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"
+
 function App() {
+  const [greeting, setGreetingValue] = useState('')
+  const [userAccount, setUserAccount] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  async function requestAccount(){
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+    }
+
+    async function getBalance() {
+      if (typeof window.ethereum !== 'undefined') {
+        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(tokenAddress, Token.abi, provider)
+        const balance = await contract.balanceOf(account);
+        console.log("Balance: ", balance.toString());
+      }
+    }
+
+    async function sendCoins() {
+      if (typeof window.ethereum !== 'undefined') {
+        await requestAccount()
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+        const transaction = await contract.transfer(userAccount, amount);
+        await transaction.wait();
+        console.log(`${amount} Coins successfully sent to ${userAccount}`);
+      }
+    }
+
+    async function fetchGreeting(){
+        if (typeof window.ethereum !== 'undefined') {
+          const provider = new ethers.providers.Web3Provider(window.ethereum)
+          // console.log({ provider })
+          const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+          try {
+            const data = await contract.greet()
+            console.log('data: ', data)
+          } catch (err) {
+            console.log("Error: ", err)
+          }
+        }
+      }
+
+    async function setGreeting(){
+        if (!greeting) return
+        if (typeof window.ethereum !== 'undefined') {
+          await requestAccount()
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          console.log({ provider })
+          const signer = provider.getSigner()
+          const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+          const transaction = await contract.setGreeting(greeting)
+          // setGreetingValue('')
+          await transaction.wait()
+          // fetchGreeting()
+        }
+      }
+
+
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <h1>APML ETHER BANK</h1>
+        <br/>
+        <h2>Set a message for User:</h2>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        <button onClick={setGreeting}>Set Greeting</button>
+        <br />
+        <br />
+        
+        <h2>Check Balance & Send Coins:</h2>
+        <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
+        
+        <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
+        <button className='btn1' onClick={getBalance}>Get Balance</button>
+        <button className='btn2' onClick={sendCoins}>Send Coins</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
